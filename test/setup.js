9fdaf835a15f5061efc0ac08609ccb2c9d663003
Mohamed Elssaffy <ma7amad123410000@gmail.com>
2021-05-31 01:20:25 +0200

    mongoose Promise

--- test/setup.js
+++ test/setup.js
@@ -2,6 +2,8 @@ jest.setTimeout(50000);
 require("../models/User");
 
 const mongoose = require("mongoose");
+
 const keys = require("../config/keys");
 
+mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI, { useMongoClient: true });
